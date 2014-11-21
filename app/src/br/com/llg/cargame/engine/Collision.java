cb6717a79783394f043c43ea0a9782eb0a8bec0b
leandrovianna <leandrovianna50@gmail.com>
2014-11-21 15:19:40 -0200

    Organizando o repositorio

---
+++ app/src/br/com/llg/cargame/engine/Collision.java
@@ -0,0 +1,15 @@
+package br.com.llg.cargame.engine;
+
+public class Collision {
+
+	public static boolean isCollided(GameObject a, GameObject b) {
+		
+		if (a.x >= b.x && a.x <= (b.x + b.width))
+			if (a.y >= b.y && a.y <= (b.y + b.height))
+				return true;
+		
+		return false;
+	
+	}
+	
+}

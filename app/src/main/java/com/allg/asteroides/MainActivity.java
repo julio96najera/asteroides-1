15cdcb75f0ca624444b8ce29c959e50ebf784b1f
leandrovianna <leandrovianna50@gmail.com>
2014-12-15 17:19:41 -0200

    Ajustando os temas

--- app/src/main/java/com/allg/asteroides/MainActivity.java
+++ app/src/main/java/com/allg/asteroides/MainActivity.java
@@ -2,7 +2,6 @@ package com.allg.asteroides;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,7 +24,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 

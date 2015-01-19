0cf433b07de677f76e4cba516812e330bcf340a2
GeovRodri <geovanyscv@gmail.com>
2015-01-19 19:54:44 -0200

    Começando a criar os creditos.

--- app/src/main/java/com/allg/asteroides/CreditsActivity.java
+++ app/src/main/java/com/allg/asteroides/CreditsActivity.java
@@ -1,42 +1,24 @@
 package com.allg.asteroides;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 
-public class MainActivity extends Activity {
+public class CreditsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Colocando a Activity em tela inteira
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.credits);
     }
 
-    public void start(View v){
-        Intent intent = new Intent(this, Level1Activity.class);
-        startActivity(intent);
-    }
-
-    public void credits(View v){
-        Intent intent = new Intent(this, CreditsActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_credits, menu);
         return true;
     }
 
@@ -45,13 +27,13 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        /*int id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-        */
+
         return super.onOptionsItemSelected(item);
     }
 }
